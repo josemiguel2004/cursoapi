0fc7d96ce11c22e4169d38b5197f45e8450400b3
Miguel Jose Albano da Silva <miguel.silva89@etec.sp.gov.br>
2022-03-17 17:12:46 -0300

    Criação das tabelas e configuração das propriedades

---
+++ src/main/java/br/com/miguel/cursoapi/model/Aluno.java
@@ -0,0 +1,4 @@
+package br.com.miguel.cursoapi.model;
+
+public class Aluno {
+}
---
+++ src/main/resources/db/migration/V001__Criar_Tabela_Curso.sql
@@ -0,0 +1,9 @@
+Create table curso(
+id int not null primary key auto_increment,
+nomecurso varchar(150)
+);
+
+insert into curso(nomecurso) values('Administração');
+insert into curso(nomecurso) values('Contabilidade');
+insert into curso(nomecurso) values('Desenvolvimento de sistemas');
+insert into curso(nomecurso) values('Química');
